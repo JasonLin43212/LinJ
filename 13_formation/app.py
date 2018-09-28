2a2c0af5413fbd040bb74ef70936e98015e936b5
Jason Lin <jasonlin43212@gmail.com>
2018-09-28 05:24:41 -0400

    did homework assignment 13

---
+++ 13_formation/app.py
@@ -0,0 +1,22 @@
+#Jason Lin
+#SoftDev1 pd07
+#K13 -- Echo Echo Echo
+#2018-09-28
+
+from flask import Flask,render_template,request
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return render_template("formThing.html")
+
+
+@app.route("/auth")
+def authenticate():
+    return render_template("answer.html",
+                           username=request.args["username"],
+                           method=request.method)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
