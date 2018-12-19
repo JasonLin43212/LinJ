c7bb0bb73c905c47ffce082e03e547f767c20f96
Jason Lin <jasonlin43212@gmail.com>
2018-12-18 19:13:21 -0500

    did assingment

---
+++ 28_js/funky_town.js
@@ -0,0 +1,43 @@
+/*
+Team LearLination -- Amit Narang and Jason Lin
+SoftDev1 pd7
+K28 -- Sequential Progression
+2018-12-18
+*/
+
+
+var fibonacci = (n) => {
+  // 0 and 1 are less than 2 and return themselves
+  if (n < 2){
+    return n;
+  }
+  return fibonacci(n-1) + fibonacci(n-2);
+}
+
+var gcd = (a,b) => {
+  // gcd(0,0) is undefined
+  if (a == 0 && b == 0){
+    return undefined;
+  }
+
+  // Now Euclidean Algorithm
+  var x = Math.max(a,b);
+  var y = Math.min(a,b);
+
+  while (true){
+    if (y == 0){
+      return x;
+    }
+    else {
+      temp = y
+      y = x % y;
+      x = temp;
+    }
+  }
+}
+
+var randomStudent = () => {
+  var students = ["Amit","Jason","Clara","Jerry","Puneet","Runmin","Ivan","Rubin"];
+  var randomIndex = Math.floor(Math.random()*students.length);
+  return students[randomIndex];
+}
