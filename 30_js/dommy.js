b95f38fb2602b898e8c6774276797b3bf0d17f3e
Jason Lin <jasonlin43212@gmail.com>
2018-12-21 12:59:44 -0500

    faster fib

--- 30_js/dommy.js
+++ 30_js/dommy.js
@@ -1,15 +1,6 @@
 // Lo, what is this? Could it be a VALUE-ADDED-KEY2SUCCESS?!?!
-/*
-LearLination -- Amit Narang and Jason Lin
-SoftDev1 pd<p7
-K30 -- Sequential Progression III: Season of the Witch
-2018-12-20
-*/
-
 
 var changeHeading = function(e) {
-  // Changes heading to Hello World if the user activates the
-  // mouseout event and will set it to the list element otherwise
     var h = document.getElementById("h");
     if (e.type === "mouseout"){
       h.innerHTML = "Hello World!";
@@ -20,7 +11,6 @@ var changeHeading = function(e) {
 };
 
 var removeItem = function(e) {
-  // Removes the target and sets the heading back to Hello World
   e.target.remove();
   var h = document.getElementById("h");
   h.innerHTML = "Hello World!";
@@ -49,15 +39,20 @@ return 1;
     } else {
 return fib(n-1) + fib(n-2);
     }
+
 };
 
 var addFib = function(e){
     // Check # of li items in list , return fib at that ind
     var l = document.getElementById("fiblist");
-    var ll = l.getElementsByTagName("li").length;
+    var ll = l.getElementsByTagName("li");
     var it = document.createElement("li");
-    it.innerHTML = fib(ll);
-    //fixLi(it);
+    if (ll.length <= 1){
+      it.innerHTML = ll.length;
+    }
+    else {
+      it.innerHTML = parseInt(ll[ll.length-1].innerHTML)+parseInt(ll[ll.length-2].innerHTML);
+    }
     l.appendChild(it);
 
 };
